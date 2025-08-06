357669eeed6a4d0a429330a2e612bca582dcfbfc
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-08-06 12:37:32 +0100

    views now present

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library
 
 # Create your views here.
 def book(request):
-    return render(request, 'list_books.html')
+    return render(request, 'relationship_app/list_books.html', 'Book.objects.all()')
 
 class LibararyView(ListView):
     template_name= 'library_detail.html'
