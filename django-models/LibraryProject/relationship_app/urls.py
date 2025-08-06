37e26b56b14e464d254d9be5e9236e0ac39db3bd
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-08-06 14:02:38 +0100

    user authentication added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-from .views import list_books, LibraryDetailView
+
+from .views import list_books, LibraryDetailView, UserRegisterView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     
-    path('list_books/', views.list_books, name='book_list'),
-    path('LibraryDetailView/', views.LibraryDetailView.as_view(), name='Library')
+    path('list_books/', list_books, name='book_list'),
+    path('LibraryDetailView/', LibraryDetailView.as_view(), name='Library'),
+    path('register/', UserRegisterView.as_view(), name= "register"),
+    path('login/', auth_views.LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'relationship_app/logout.html'), name ='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -12,4 +15,9 @@ class LibraryDetailView(ListView):
     template_name= 'relationship_app/library_detail.html'
     model = Library
     context_object_name = 'book'
+
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
     
