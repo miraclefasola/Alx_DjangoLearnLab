85d312d6207868572dcaed907a4e0c030df55494
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-08-17 00:45:12 +0100

    Token authentication tied to user on sign up

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 class Book(models.Model):
     title= models.CharField(max_length=300)
     author= models.CharField(max_length=200)
+
+
+User= get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .serializers import BookSerializer
 from rest_framework import generics, viewsets
 from .models import Book
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
@@ -11,4 +12,4 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset= Book.objects.all()
     lookup_field= 'pk'
-    #permission_classes= [IsAuthenticatedOrReadOnly]
+    permission_classes= [IsAuthenticatedOrReadOnly, TokenAuthentication]
