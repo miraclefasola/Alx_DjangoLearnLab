00ad12cd64958cca0500013430b6d97da2292882
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-08-17 00:03:03 +0100

    List view created

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
+from .models import Book
 
-# Create your views here.
+class BookList(ListAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
