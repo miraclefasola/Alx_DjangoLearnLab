084af8e309a5802ab5528923e09541236ac078ee
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-08-17 00:22:14 +0100

    Model Viewset added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .serializers import BookSerializer
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset= Book.objects.all()
+    lookup_field= 'pk'
+    #permission_classes= [IsAuthenticatedOrReadOnly]
