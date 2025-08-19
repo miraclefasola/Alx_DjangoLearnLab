b4fd538db38e865960af33f3144186349328e9dc
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-08-20 00:03:24 +0100

    advanced api config

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,8 +9,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 # router.register(r'authors', AuthorView, basename='author_view')
 urlpatterns = [path('books/', BookListView.as_view(), name="book_list"),
                path("books/create",  BookCreateView.as_view(), name="book_create"),
-               path('books/<int:pk>/', BookRetrieveView.as_view(), name='book_retrieve'),
-               path('books/<int:pk>/delete', BookDeleteView.as_view(), name='book_delete'),
+               path('books/detail/<int:ok>/', BookDetailView.as_view(), name= 'detail_view')
+               path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book_update'),
+               path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
                path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
                     
 
