6ea1131cfcf21885abe46c69a27f81204b293360
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-08-19 21:34:25 +0100

    advanced api config

---
+++ advanced_api_project/api/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router= DefaultRouter()
+router.register(r"books", BookView, basename='book_view')
+router.register(r'author', AuthorView, basename='author_view')
+urlpatterns = [path('api/', include (router.urls))
+
+]
--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Book, Author
+from rest_framework.viewsets import ModelViewSet
+from .serializers import BookSerializer, AuthorSerializer
 
+class BookView(ModelViewSet):
+    queryset= Book.objects.all()
+    serializer_class=BookSerializer
 
-#pip install djangorestframework
+class AuthorView(ModelViewSet):
+    queryset= Author.objects.all()
+    serializer_class= AuthorSerializer
